3f4ee6b1fa87bdb3ef31e414579aa4262e36c7dd
MatheusFermino <mh9000785@gmail.com>
2021-10-11 16:54:01 -0300

    criação da tabela usuário

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nick');
+            $table->string('full_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('picture')->default('storage\default\profile.png');
+            $table->string('local')->default('Brazil');
+            $table->string('language')->default('pt-br');
+            $table->integer('level')->default('99');
+            $table->dateTime('suspension_beginning')->nullable();//criado_em e atualizado_em
+            $table->dateTime('suspension_end')->nullable();//criado_em e atualizado_em
+            $table->softDeletes();
+            $table->float('rate',1,1)->nullable();
+            $table->boolean('verified')->default(false);
+            $table->boolean('especialist')->default(false);
+            $table->dateTime('birth');
+            $table->longText('bio')->nullable();
+            $table->dateTime('email_verified_at')->nullable();//criado_em e atualizado_em
             $table->rememberToken();
             $table->timestamps();
         });
